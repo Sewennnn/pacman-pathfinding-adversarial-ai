3644fa0271ade4bdb3a1bd2712799c3f1e528fa3
Sewennnn <juliachoong15@gmail.com>
2024-09-04 23:49:27 +0800

    push

--- solvers/q1c_solver.py
+++ solvers/q1c_solver.py
@@ -80,9 +80,20 @@ def astar_loop_body(problem: q1c_problem, astarData: AStarData):
 
 def astar_heuristic(state, goals):
     pacman_position, remaining_food = state
-    rows = len(set([f[0] for f in remaining_food]))  
-    cols = len(set([f[1] for f in remaining_food]))  
-    return rows + cols
+
+    if len(remaining_food) > 0:
+        return 1
+    
+    return 0
+#     rows = len(set([f[0] for f in remaining_food]))  
+#     cols = len(set([f[1] for f in remaining_food]))  
+#     return rows + cols
+
+
+
+# if len(state.getFood().asList()) > 0:
+#         return 1
+#     return 0
     
     
 
